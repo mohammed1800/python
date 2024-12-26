5d9a299cd4d3c1530699bf657e28595e48cb2a11
mohammed1800 <119121182+mohammed1800@users.noreply.github.com>
2024-12-26 23:02:23 +0530

    Create p7 the indices of all the occurrences return -1.py

---
+++ p7 the indices of all the occurrences return -1..py
@@ -0,0 +1,13 @@
+def find_occurrences(str1, str2):
+    indices = []
+    index = str1.find(str2)
+    while index != -1:
+        indices.append(index)
+        index = str1.find(str2, index + 1)
+    return indices if indices else -1
+
+str1 = input("Enter the first string: ")
+str2 = input("Enter the second string: ")
+
+result = find_occurrences(str1, str2)
+print(result)
